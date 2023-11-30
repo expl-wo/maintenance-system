4fd554355cc15df5b40a41b104194364e480b837
tangjiacheng <liaoyiprjk@163.com>
2023-11-30 08:32:33 +0800

    语法修改，主计划节点编辑页面

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
 		      "allowedNames": ["that","self","_this"] // Allow `const self = this`; `[]` by default
 		    }
 		],
-		// "@typescript-eslint/no-empty-function":"off",
+		"@typescript-eslint/no-empty-function":"off",
 		"@typescript-eslint/ban-ts-comment": "off",
 		// "off"或者0    关闭规则关闭
 		// "warn"或者1    在打开的规则作为警告（不影响退出代码）
